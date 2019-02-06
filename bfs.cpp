1d7632ea83d6e9c21035a59a63133e31862f73c9
apaun <abhijeet.paunikar@gmail.com>
2019-02-06 10:09:29 +0530

    Added dfs

---
+++ bfs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include"graph.h"
+using namespace std;
+
+vector<int> *adj;
+
+int main()
+{
+    int V = 4;
+    
+
+    return 0;
+}
---
+++ dfs.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include"graph.h"
+using namespace std;
+
+vector<int> *adj;
+
+void addEdge(int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void dfsUtil(vector<bool> &visited, int u)
+{
+    visited[u] = true;
+    cout << u << " ";
+    vector<int>::iterator i;
+    for(i = adj[u].begin(); i != adj[u].end(); ++i)
+    {
+        if (visited[*i] == false)
+            dfsUtil(visited, *i);
+    }
+
+}
+
+void dfs(int V)
+{
+    vector<bool> visited(V, false);
+    for(int i = 0; i < V; i++)
+    {
+        if (visited[i] == false)
+            dfsUtil(visited, i);
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Graphs" << endl;
+    int V = 4;
+    adj = new vector<int>[V];
+    addEdge(0, 1);
+    addEdge(0, 2);
+    addEdge(1, 2);
+    addEdge(2, 0);
+    addEdge(2, 3);
+    addEdge(3, 3);
+    dfs(V);
+    return 0;
+}
